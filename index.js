0e71ac5b97217bbbfa7f2fb626c456210b1900f9
njmbb8 <nmcgr001@gmail.com>
2021-06-22 18:57:04 -0400

    added brick and game classes. restructured code so that everything is occurring in the game class

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-function generateBricks(){
-    const numberOfBricks = 60; //6 bricks can fit in each column 10 bricks in each row
-    let brickContainer = document.getElementById("brickContainer"); //store the #brickcontainer element in a variable
-    for( let i = 0; i < numberOfBricks; i++){
-        let brick = document.createElement('div'); //create a div element and assign it to a variable
-        brick.classList.add('brick'); //add the brick class to the element, telling the computer that this is a brick
-        let red = Math.floor(Math.random() * 255); //generate random red value
-        let blue = Math.floor(Math.random() * 255); //generate random blue value
-        let green = Math.floor(Math.random() * 255); //generate random green value
-        brick.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; //use the previously generated values to set the brick to a random color
-        brickContainer.appendChild(brick); //place the brick in the brickContainer which will make it show on screen
-    }
-}
-
-/*The following function tells the DOM to listen for a key
-  to be pressed, and if it is, move our paddle in the co-
-  responding direction*/
-function bindKeys(paddle) {
-    document.addEventListener('keydown', function(e){
-        switch (e.key) {
-            case "ArrowLeft":
-                paddle.moveLeft(); 
-                break;
-            case "ArrowRight":
-                paddle.moveRight();
-                break;
-            case "ArrowUp":
-                paddle.moveUp();
-                break;
-            case "ArrowDown":
-                paddle.moveDown();
-                break;
-            default:
-                break;
-        }
-    })
-}
-
 class baseElement{
     constructor(id){
         this.node = document.getElementById(id);
@@ -106,6 +68,67 @@ class projectile extends baseElement{
     }
 }
 
-const gamePaddle = new paddle('paddle'); //instantiate a variable as a new instance of paddle
-generateBricks(); //generate the bricks
-bindKeys(gamePaddle); //establish controls
+class brick extends baseElement{
+    constructor(id){
+        super(id);
+    }
+    isDisappeared = false;
+    disappear(){
+        this.node.style.backgroundColor = 'black';
+        this.isDisappeared = true;
+    }
+}
+
+class game extends baseElement{
+    constructor(id){
+        super(id);
+        this.paddle = new paddle('paddle');
+        this.projectile = new projectile('projectile');
+        this.generateBricks();
+        this.bindKeys(this.paddle);
+    }
+    
+    generateBricks(){
+        const numberOfBricks = 60; //6 bricks can fit in each column 10 bricks in each row
+        let brickContainer = document.getElementById("brickContainer"); //store the #brickcontainer element in a variable
+        for( let i = 0; i < numberOfBricks; i++){
+            let brick = document.createElement('div'); //create a div element and assign it to a variable
+            brick.classList.add('brick'); //add the brick class to the element, telling the computer that this is a brick
+            let red = Math.floor(Math.random() * 255); //generate random red value
+            let blue = Math.floor(Math.random() * 255); //generate random blue value
+            let green = Math.floor(Math.random() * 255); //generate random green value
+            brick.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`; //use the previously generated values to set the brick to a random color
+            brickContainer.appendChild(brick); //place the brick in the brickContainer which will make it show on screen
+        }
+    }
+    /*The following function tells the DOM to listen for a key
+    to be pressed, and if it is, move our paddle in the co-
+    responding direction*/
+    bindKeys(paddle) {
+        document.addEventListener('keydown', function(e){
+            switch (e.key) {
+                case "ArrowLeft":
+                    paddle.moveLeft(); 
+                    break;
+                case "ArrowRight":
+                    paddle.moveRight();
+                    break;
+                case "ArrowUp":
+                    paddle.moveUp();
+                    break;
+                case "ArrowDown":
+                    paddle.moveDown();
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
+}
+
+const gameContext = new game('game');
+
+
+    /*The following function will update the screen with changes
+    that have been made to any elements of the game(such as the
+    projectile moving on the screen*/
